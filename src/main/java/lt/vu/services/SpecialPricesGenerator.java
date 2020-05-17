cc5d42e1b9e1d0127006a1e01345379d064e53e0
ievavizg <ievaviz92@gmail.com>
2020-05-17 19:41:20 +0300

    @Alternative

--- src/main/java/lt/vu/services/SpecialPricesGenerator.java
+++ src/main/java/lt/vu/services/SpecialPricesGenerator.java
@@ -7,15 +7,15 @@ import java.util.concurrent.ThreadLocalRandom;
 
 @ApplicationScoped
 @Alternative
-public class PriceGenerator implements Generator {
+public class SpecialPricesGenerator implements Generator{
 
     public Double generateNumber() {
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
         }
-        Double min = 0.0;
-        Double max = 5.0;
+        Double min = 10.0;
+        Double max = 30.0;
         double generatedPrice = ThreadLocalRandom.current().nextDouble(min, max);
         double generatedPriceRounded = Math.round(generatedPrice * 100.0) / 100.0;
         return generatedPriceRounded;
